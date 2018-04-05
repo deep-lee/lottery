6dafa36a1af8f30b9d5bcc488c6d1f342391055b
李冬 <dong@192.168.0.101>
2018-04-05 23:33:14 +0800

    1

--- back/register.php
+++ back/register.php
@@ -13,18 +13,25 @@ if (check_user_login_out_of_time() == false) {
         $username = $_GET['username'];
         $password = $_GET['password'];
 
-        $sql = "insert into admin(username, password) values(
-            '$username', '$password'
-        )";
-
-        // echo $sql;
-
-        $result = mysql_query($sql);
-        if ($result == false) {
-            $returnData['rt_code'] = 0;
+        $login_user_id = get_login_user_id();
+        if ($login_user_id == 1) {
+            $sql = "insert into admin(username, password) values(
+                '$username', '$password'
+            )";
+    
+            // echo $sql;
+    
+            $result = mysql_query($sql);
+            if ($result == false) {
+                $returnData['rt_code'] = 0;
+            } else {
+                $returnData['rt_code'] = 1;
+            }
         } else {
-            $returnData['rt_code'] = 1;
+            $returnData['rt_code'] = 3;
         }
+
+        
 
     } else {
         $returnData['rt_code'] = -1;
