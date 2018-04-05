64f374ef46ec4e25e552287ba7f9fc82a1854e4d
李冬 <dong@192.168.0.101>
2018-04-05 16:53:47 +0800

    1

--- back/get_app_data.php
+++ back/get_app_data.php
@@ -17,6 +17,8 @@ if (check_user_login_out_of_time() == false) {
   $search_text = $_GET['search_text'];
   // $search_text = urldecode($search_text);
 
+  echo $search_text;
+
   $login_user_id = get_login_user_id();
 
   // echo $limit." ".$offset." ".$search_text." ".$start_index;
