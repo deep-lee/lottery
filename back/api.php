4623b48e28fa3990d7eb215b265d366d4ec9b272
李冬 <dong@192.168.0.100>
2018-04-25 22:40:19 +0800

    1

--- back/api.php
+++ back/api.php
@@ -2,46 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-include 'conn_memcached.php';
-include 'My.php';
-
-$db_user = 'root';
-$db_pwd = 'idc@bt1113';
+include 'conn.php';
 
 if (isset($_GET["appid"])) {
-  //面向对象方式
-  // $mysqli = new mysqli($db_host, $db_user, $db_pwd, $db_name);
   $appid = $_GET["appid"];
 
-  // echo $appid;
+  $sql = "select show_url as is_wap, url as wap_url, is_update, update_url from lottery where appid='$appid'";
 
-  //缓存服务器中，都是键值对，这里我们设定唯一的键
-  $key = md5($appid);
+  $result = mysql_query($sql);
 
-  $cache_result = array();
-  //根据键，从缓存服务器中获取它的值
-  $cache_result = $mem->get($key);
-  //如果存在该键对应的值，说明缓存中存在该内容
-  $dbConnection = new PDO('mysql:dbname=mydb2;host=127.0.0.1;charset=utf8', $db_user, $db_pwd);
-
-  $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-  $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // $data_result = array();
-  if($cache_result){
-    // 已经缓存了
-    echo "get from memcached";
-    $data_result=$cache_result;
-  } else {
-    echo "get from mysql";
-    $stmt = $dbConnection->prepare("SELECT * FROM lottery WHERE appid = :appid");
-    $stmt->execute(array(':appid' => $appid));
-    foreach ($stmt as $row) {
-      // echo $row;
-      $data_result = $row;
-      // $data_result['code'] = 200;
-      // $data_result['msg'] = '';
-    }
-    $mem->set($key, $data_result, MEMCACHE_COMPRESSED, 3600);
+  while( $row = mysql_fetch_array($result) ){
+    $data_result = $row;
   }
 
   // update request number
